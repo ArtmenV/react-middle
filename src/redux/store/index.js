a0ba87191f4c665488eae7f263950d54dc33bc87
Artmen_V <viliutich.artem@gmail.com>
2019-07-13 16:13:38 +0700

    add react devtools

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers";
 import createSagaMiddleware from 'redux-saga'
 import mySaga from '../sagas/postsSaga.js'
 
 const sagaMiddleware = createSagaMiddleware()
 
-const enhancer = applyMiddleware(sagaMiddleware)
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
 
 const store = createStore(rootReducer, enhancer);
 window.store = store;
